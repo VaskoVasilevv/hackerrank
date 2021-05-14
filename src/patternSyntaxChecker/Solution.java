c219099913fe6c9dd8005521e5226585ee3fcda0
VaskoVasilevv <n1ggazz@abv.bg>
2021-05-14 15:06:27 +0300

    new exercise

---
+++ src/patternSyntaxChecker/Solution.java
@@ -0,0 +1,26 @@
+package patternSyntaxChecker;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            //Write your code
+            try {
+                String pattern = in.nextLine();
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases --;
+        }
+    }
+
+}
