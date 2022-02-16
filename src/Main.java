a68dacd3f13df93e854ab3fc2a72bd7a9d35e9aa
VaskoVasilevv <n1ggazz@abv.bg>
2022-02-16 17:47:27 +0200

    new exercise

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        int j = 100;
-        for (int i = 1; i <= j; i += 2) {
-            System.out.printf("%d,%d,%d,%d,", i, i + 1, j - i, j - i - 1);
+//        int j = 100;
+//        for (int i = 1; i <= j; i += 2) {
+//            System.out.printf("%d,%d,%d,%d,", i, i + 1, j - i, j - i - 1);
+//        }
+//        for (int i = 0; i < arr.lenght(); i++) {
+//
+//        }
+
+    }
+
+    void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
