2c6fc415654aaee087b29eb81a76c81b10a8ddf2
VaskoVasilevv <n1ggazz@abv.bg>
2022-02-18 17:11:59 +0200

    new exercise

---
+++ src/asd.java
@@ -0,0 +1,7 @@
+public class asd {
+    public static void main(String[] args) {
+        String asd= "roulette";
+
+        System.out.println(asd.substring(1).concat(asd.substring(0,1)));
+    }
+}
