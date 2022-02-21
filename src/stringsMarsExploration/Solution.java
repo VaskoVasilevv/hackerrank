dd147a9af3676e4b9ff2987e874b2e98bbff0c85
VaskoVasilevv <n1ggazz@abv.bg>
2022-02-21 22:56:00 +0200

    new exercise

---
+++ src/stringsMarsExploration/Solution.java
@@ -0,0 +1,63 @@
+package stringsMarsExploration;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int marsExploration(String s) {
+        // Write your code here
+
+        int n = s.length() / 3;
+        int out = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append("SOS");
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != sb.charAt(i)) {
+                out++;
+            }
+        }
+        return out;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
